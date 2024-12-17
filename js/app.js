461472648fb3c6d9af7aae6018f149b4f33244e4
Christian Bustos <christianbus37@gmail.com>
2024-12-17 11:01:02 -0300

    primer commit

---
+++ js/app.js
@@ -0,0 +1,4 @@
+const result = document.querySelector(".result");
+const form = document.querySelector(".get-weather");
+const inputCity = document.querySelector("#city");
+const inputCountry = document.querySelector("#country");
